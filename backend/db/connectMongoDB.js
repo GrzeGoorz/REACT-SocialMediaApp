e107874546ba58cc7a3ee5a6b0deee150073b3cf
Grze Gorz <grzegorz096@gmail.com>
2026-05-06 23:43:47 +0200

    1 - Project setup

---
+++ backend/db/connectMongoDB.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectMongoDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default connectMongoDB;
